f8a8a5e65be748a09d7b7a54b858aaa4fe2140a0
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-19 22:41:16 -0400

    Set up bodyParser and routes for users, tweets

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 const app = express();
 const db = require("./config/keys").mongoURI;
@@ -7,7 +10,12 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World!!!!!!!!"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
+  
+app.get("/", (req, res) => res.send("Hello World!!!!!!!!"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
