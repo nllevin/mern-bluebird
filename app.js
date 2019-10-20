66cf6a4bc64c3bc051a133e0113c78c5a2664450
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-20 10:02:23 -0400

    Add server:debug script to use DevTools for Node

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
 const bodyParser = require("body-parser");
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
 const app = express();
-const db = require("./config/keys").mongoURI;
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
