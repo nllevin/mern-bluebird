c468a9a1c844481c6cf026518d884996dda1f170
nllevin <50681582+nllevin@users.noreply.github.com>
2019-10-20 10:36:44 -0400

    Add User schema

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserScehma = new Schema ({
+  handle: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
